5a31ba956b24bf949f7870ed4b31c666d3feb94d
lukyanenko01 <hr.vladimir01@gmail.com>
2022-08-12 23:04:35 +0300

    eee2341

--- vvv124/ViewController.swift
+++ vvv124/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("jopa")
         // Do any additional setup after loading the view.
     }
 
